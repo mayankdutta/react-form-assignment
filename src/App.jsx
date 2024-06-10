433e3f15f003330c1e66e0c70b455d2c40f81cb2
mayank dutta <smayankdutt@gmail.com>
2024-06-10 17:21:23 +0530

    data loaded in API
    in tabular form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useEffect, useState } from "react";
 
 function App() {
+  const [formData, setFormData] = useState([]);
+  const [formHeader, setFormHeader] = useState([]);
+
   useEffect(() => {
     async function getData() {
       try {
         const data = await fetch("https://fakestoreapi.com/products");
         const modified_data = await data.json();
+
+
+        let tempFormHeader = [];
+        for (let i in modified_data[0]) {
+          tempFormHeader.push(i);
+        }
+
         console.log(modified_data);
+
+        setFormHeader(tempFormHeader);
+        setFormData(modified_data);
       } catch (error) {
         console.log(error.message);
       }
@@ -15,11 +28,31 @@ function App() {
     getData();
   }, []);
 
-  return (
-    <>
-      <div> Hello world </div>
-    </>
-  );
+  if (formData.length) {
+    return (
+      <table>
+        <tr>
+          {formHeader.map((header) => {
+            if (header === "rating" || header === "image")
+              return <td key={header}></td>;
+            return <th key={header}> {header}</th>;
+          })}
+        </tr>
+
+        {formData.map((data, i) => (
+          <tr key={i}>
+            {Object.keys(data).map((value) => {
+              if (value === "rating" || value === "image")
+                return <td key={value.id}></td>;
+              else return <td key={value.id}>{data[value]}</td>;
+            })}
+          </tr>
+        ))}
+      </table>
+    );
+  } else {
+    return <> Loading .... </>;
+  }
 }
 
 export default App;
