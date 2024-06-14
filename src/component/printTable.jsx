227740298e22f97ce76a4377c8e3f0ed31fb6750
Dutta <mayank.dutta@accenture.com>
2024-06-14 14:54:17 +0530

    introduced useReducer
    working fine as of now, few developments needed

--- src/component/printTable.jsx
+++ src/component/printTable.jsx
@@ -1,41 +1,53 @@
 /* eslint-disable react/prop-types */
-import { useContext, useState } from "react";
+import { useContext, useReducer, useState } from "react";
 import "./printTable.css";
 import { APIDataContext } from "../contexts/apiData";
 import TableBody from "./table/body";
 import TableHeader from "./table/header";
 
+const tableReducer = (state, action) => {
+  const { col, id } = action;
 
+  switch (action.type) {
+    case "sortInc": {
+      return typeof col == "string"
+        ? [...state].sort((a, b) => {
+            if (a[col] < b[col]) return -1;
+            if (a[col] > b[col]) return 1;
+            return 0;
+          })
+        : [...state``].sort((a, b) => a[col] - b[col]);
+    }
 
+    case "sortDec": {
+      return typeof col == "string"
+        ? [...state].sort((a, b) => {
+            if (a[col] < b[col]) return 11;
+            if (a[col] > b[col]) return -1;
+            return 0;
+          })
+        : [...state].sort((a, b) => -a[col] + b[col]);
+    }
+
+    case "delete":
+      console.log("iam here, id: ", id);
+      return state.filter((i) => i.id !== id);
+
+    default:
+      throw new Error("unhandled action ", action.type);
+  }
+};
 
 const PrintTable = () => {
   const { formData } = useContext(APIDataContext);
   const [selectedOptions, setSelectedOptions] = useState([]);
   const { setFormData } = useContext(APIDataContext);
 
-  const sortInc = (col) => {
-    if (typeof col == "string") {
-      setFormData(
-        [...formData].sort((a, b) => {
-          if (a[col] < b[col]) return -1;
-          if (a[col] > b[col]) return 1;
-          return 0;
-        })
-      );
-    } else setFormData([...formData].sort((a, b) => a[col] - b[col]));
-  };
+  const [state, dispatch] = useReducer(tableReducer, formData);
 
-  const sortDec = (col) => {
-    if (typeof col == "string") {
-      setFormData(
-        [...formData].sort((a, b) => {
-          if (a[col] < b[col]) return 1;
-          if (a[col] > b[col]) return -1;
-          return 0;
-        })
-      );
-    } else setFormData([...formData].sort((a, b) => -a[col] + b[col]));
-  };
+  const sortInc = (col) => dispatch({ type: "sortInc", col });
+  const sortDec = (col) => dispatch({ type: "sortDec", col });
+  const deleteRow = (id) => dispatch({ type: "delete", id });
 
   const filterStuff = (data) => {
     if (selectedOptions.length == 0) {
@@ -43,17 +55,9 @@ const PrintTable = () => {
     }
     return data.filter((d) => {
       return selectedOptions.find((value) => {
-        // console.log(
-        //   `value label: ${value.label}, category: ${d.category}`
-        // );
         return value.label === d.category.toUpperCase();
       });
     });
-  };
-
-  const deleteRow = (id) => {
-    // console.log(`${id} is pressed`);
-    setFormData(formData.filter((i) => i.id !== id));
   };
 
   return (
@@ -70,7 +74,7 @@ const PrintTable = () => {
         </thead>
 
         <tbody>
-          {filterStuff(formData).map((data, i) => (
+          {filterStuff(state).map((data, i) => (
             <tr key={i}>
               <TableBody data={data} deleteRow={deleteRow} />
             </tr>
